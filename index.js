f0bf4004e53592e2887b3230b844fce67559571c
jennj <jennjenn93@gmail.com>
2022-04-30 16:22:10 -0500

    creating view employees function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const connection = require('./db/connection');
 const consoleTable = require('console.table');
+const db = require('./db/connection');
 
 
 const startOptions = () => {
@@ -50,5 +51,51 @@ const startOptions = () => {
         })
 };
 
-const viewEmployees =
+const viewEmployees = () => {
+    const sql = `SELECT employees.id,
+                        employees.first_name,
+                        employees.last_name,
+                        roles.title AS title,
+                        roles.salary AS salary,
+                        departments.name AS department,
+                        CONCAT (manager.first_name, " ", manager.last_name) AS manager
+                FROM employees
+                LEFT JOIN roles ON employees.role_id = roles.id
+                LEFT JOIN departments ON roles.department_id = departments.id
+                LEFT JOIN employees manager ON employees.manager_id = manager.id`;
+    
+    db.query(sql, (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        console.table(rows);
+        return startOptions();
+    });
+};
+
+const viewRoles = () => {
+
+}
+
+const viewDepartments = () => {
+
+}
+
+const updateEmployeeRole = () => {
+
+}
+
+const addEmployee = () => {
+
+}
+
+const addRole = () => {
+
+}
+
+const addDepartment = () => {
+
+}
+
+module.exports = startOptions;
 //add the rest of the prompts
