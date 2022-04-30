97570ba504fe5d4a7c19fe4f47c8f1f415a0d0f8
jennj <jennjenn93@gmail.com>
2022-04-30 13:18:41 -0500

    added option prompts

--- index.js
+++ index.js
@@ -1,19 +1,54 @@
 const inquirer = require('inquirer');
-const connection = require('./config/connection');
+const connection = require('./db/connection');
 const consoleTable = require('console.table');
 
 
-inquirer.prompt([
-    {
-        type: 'list',
-        name: 'options',
-        message: 'Welcome to the Employee Tracker. Please select which option you would like.',
-        choices: ['View ALL employees.', 'View ALL roles.', 'View ALL departments.', 'Update employee role.', 'Add employee.', 'Add role.', 'Add department', 'Finished']
-    },
+const startOptions = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'options',
+            message: 'Welcome to the Employee Tracker. Please select which option you would like.',
+            choices: [
+                'View ALL employees',
+                'View ALL roles',
+                'View ALL departments',
+                'Update employee role',
+                'Add employee',
+                'Add role',
+                'Add department',
+                'Finished'
+            ]
+        }
+    ])
+        .then((answers) => {
+            const selectedOption = answers.toDo;
+            if (selectedOption === 'View ALL employees') {
+                viewEmployees();
+            };
+            if (selectedOption === 'View ALL roles') {
+                viewRoles();
+            };
+            if (selectedOption === 'View ALL departments') {
+                viewDepartments();
+            };
+            if (selectedOption === 'Update employee role') {
+                updateEmployeeRole();
+            };
+            if (selectedOption === 'Add employee') {
+                addEmployee();
+            };
+            if (selectedOption === 'Add role') {
+                addRole();
+            };
+            if (selectedOption === 'Add department') {
+                addDepartment();
+            };
+            if (selectedOption === 'Finished') {
+                process.finished();
+            };
+        })
+};
 
-])
-.then ((response) => {
-    
-})
-
+const viewEmployees =
 //add the rest of the prompts
