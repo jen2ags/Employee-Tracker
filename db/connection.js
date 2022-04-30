980d575af1e8aa51a594369494b577760f4833ca
jennj <jennjenn93@gmail.com>
2022-04-29 21:40:03 -0500

    adding connection.js info

---
+++ db/connection.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root';
+        password: 'Lauren93Aggie2015',
+        database: 'company'
+    },
+    console.log ('Connected to the company database')
+);
+
+module.exports = db;
