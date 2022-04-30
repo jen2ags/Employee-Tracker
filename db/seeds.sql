de672bacca1285d084fd86c199ff679f316d257a
jennj <jennjenn93@gmail.com>
2022-04-30 10:27:37 -0500

    adding more seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,30 @@
 INSERT INTO department (department_name)
-VALUES ('Engineering'), ('Marketing'), ('Design'), ('Media'), ('Production'), ('Sales');
+VALUES 
+    ('Engineering'), 
+    ('Marketing'), 
+    ('Design'), 
+    ('Media'), 
+    ('Production'), 
+    ('Sales');
 
 INSERT INTO role (title, salary, department_id)
-VALUES ('President', 300000, 1), ('Designer', 90000, 2), ('Senior Designer', 125000, 2), ('Intern', 50000. 4);
+VALUES 
+    ('President', 300000, 1), 
+    ('Designer', 90000, 2), 
+    ('Senior Designer', 125000, 2), 
+    ('Intern', 50000. 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Aaron', 'Smith', 1, ),
+    (''),
+    (''),
+    (''),
+    (''),
+    (''),
+    (''),
+    (''),
+    (''),
+    ('');
 
 /* the seeds need to be completed
